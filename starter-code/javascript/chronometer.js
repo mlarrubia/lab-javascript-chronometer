4f4cbfc5abfa2c4f947da1a88abb3686182bced5
Matthew Larrubia <mlarrubia@columbushs.com>
2019-06-19 21:21:43 -0400

    Code breaks when implementing the toggle for Start, stop... reset, split. main.js lines 62 - 100

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,49 @@
+let clock;
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+   clock = setInterval(()=>{
+     
+      this.currentTime++;
+      printSeconds();
+      printMinutes();
+    }, 1000);
+
+    // DOES NOT WORK :(
+    // setInterval(function(){this.currentTime++; console.log(this.currentTime)}, 1000);
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+
+  twoDigitsNumber(time) {
+    if(time< 10){
+      return "0" + time;
+    }
+    else {
+      return "" + time;
+    }
+  }
+
+  stopClick() {
+    clearInterval(clock);
+    this.currentTime = 0;
+    printTime();
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+  
+  splitClick() {
+
+  }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,24 +1,33 @@
-var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+let chronometer = new Chronometer();
+let chronometerTwo = new Chronometer();
+let btnLeft     = document.getElementById('btnLeft');
+let btnRight    = document.getElementById('btnRight');
+let minDec      = document.getElementById('minDec');
+let minUni      = document.getElementById('minUni');
+let secDec      = document.getElementById('secDec');
+let secUni      = document.getElementById('secUni');
+let milDec      = document.getElementById('milDec');
+let milUni      = document.getElementById('milUni');
+
 
 
 function printTime() {
-
+    printMinutes();
+    printSeconds();
 }
 
 function printMinutes() {
-
+    let minutes = chronometer.getMinutes();
+    let twoDigitMinute = chronometer.twoDigitsNumber(minutes);    
+    $('#minDec').text(twoDigitMinute[0]);
+    $('#minUni').text(twoDigitMinute[1]);
 }
 
 function printSeconds() {
-
+    let sec = chronometer.getSeconds();
+    let twoDigitSec = chronometer.twoDigitsNumber(sec); 
+    $('#secDec').text(twoDigitSec[0]);
+    $('#secUni').text(twoDigitSec[1]);
 }
 
 function printMilliseconds() {
@@ -51,10 +60,43 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    $(this).toggleClass('start stop');
+    $(this).siblings().toggleClass('reset split');
+    if($(this).hasClass('start')){
+        $(this).text('START');
+        $(this).siblings().text('SPLIT');
+        chronometer.stopClick();
+    }
+    else{
+        $(this).text('STOP');
+        $(this).siblings().text('RESET');
+        // $(this).siblings.text('RESET');
+        chronometer.startClick();        
+    }    
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+    chronometer.resetClick();
+    // $(this).toggleClass('split reset');
+    // if($(this).hasClass('split')){
+    //     $(this).text('SPLIT');
+    // }
+    // else{
+    //     $(this).text('RESET');
+    // }
 
+    $(this).toggleClass('reset split');
+    $(this).siblings().toggleClass('start stop');
+    if($(this).hasClass('reset')){
+        $(this).text('RESET');
+        $(this).siblings().text('START');
+        chronometer.stopClick();
+    }
+    else{
+        $(this).text('SPLIT');
+        $(this).siblings().text('STOP');
+        // $(this).siblings.text('RESET');
+        chronometer.startClick();        
+    }    
 });
